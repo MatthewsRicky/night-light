833c29ae31dd75b46cea30f65f4683747685d9fa
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-16 23:18:40 +0300

    Home screen

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import "../globals.css";
+
+const home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="flex items-center justify-center">home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default home;
