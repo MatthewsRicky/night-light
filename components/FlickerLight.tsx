c1accdf8df4fe2e800c78208e9fbe4bdab2750bc
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-17 23:03:50 +0300

    renaming

---
+++ components/FlickerLight.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const FlickerLight = () => {
+  return (
+    <View>
+      <Text>FlickerLight</Text>
+    </View>
+  );
+};
+
+export default FlickerLight;
