328db389527e2b35f91b148f2a8298173ec21dff
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-25 21:43:24 +0300

    Addind a color picker

---
+++ components/ColorPicker.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function ColorPicker() {
+  return (
+    <View className="p-4 border rounded-lg border-gray-300 dark:border-gray-600">
+      <Text className="text-black dark:text-white mb-2">
+        Color selection coming soon!
+      </Text>
+    </View>
+  );
+}
