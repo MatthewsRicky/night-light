4b531ee71209a28b32c50b4dd8adf0998b87fa44
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-25 21:44:18 +0300

    Adding Timer Controls

---
+++ components/TimerControl.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function TimerControl() {
+  return (
+    <View className="p-4 border rounded-lg border-gray-300 dark:border-gray-600">
+      <Text className="text-black dark:text-white mb-2">
+        Timer UI coming soon!
+      </Text>
+    </View>
+  );
+}
