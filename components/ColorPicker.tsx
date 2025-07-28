40a5d9303077e7e30fda7bdbd4edb75ac857d044
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-28 23:11:19 +0300

    Fixing imports and formatting

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
 import Slider from "@react-native-community/slider";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
 
 type Props = {
   onChange: (color: string) => void;
