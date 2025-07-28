5cc23e4c07d253908a9a41694a22a598e6e4a8df
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-28 23:11:45 +0300

    changing color to baseColor

--- components/FlickerLight.tsx
+++ components/FlickerLight.tsx
@@ -64,7 +64,7 @@ export default function FlickerLight({ baseColor }: Props) {
       style={[
         StyleSheet.absoluteFill,
         {
-          backgroundColor: color,
+          backgroundColor: baseColor,
           opacity: opacityAnim,
         },
       ]}
