afee1120cfc2139ff16aac0c8a8a5dc0d33444a4
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-30 19:36:10 +0300

    Wrapping the app in a lighting context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 // app/_layout.tsx
+import { LightingProvider } from "@/context/LightingContext";
 import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Light" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
-      <Tabs.Screen name="timer" options={{ title: "Timer" }} />
-    </Tabs>
+    <LightingProvider>
+      <Tabs>
+        <Tabs.Screen name="index" options={{ title: "Light" }} />
+        <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+        <Tabs.Screen name="timer" options={{ title: "Timer" }} />
+      </Tabs>
+    </LightingProvider>
   );
 }
