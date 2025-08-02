5e4d23cc36f6589ae859b1a8e96890a61c00604c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-02 13:35:05 +0300

    renaming

---
+++ utils/moodColors.ts
@@ -0,0 +1,20 @@
+import { Mood } from "../context/LightingContext";
+
+
+export function getMoodColors(family: Mood): [string, string] {
+  switch (family) {
+    case "cool":
+      return ["#cceeff", "#3366ff"];
+    case "green":
+      return ["#d0ffd0", "#00cc66"];
+    case "purple":
+      return ["#e0d0ff", "#9933ff"];
+    case "pink":
+      return ["#ffd6e7", "#ff69b4"];
+    case "yellow":
+      return ["#fffdd0", "#ffcc00"];
+    case "warm":
+    default:
+      return ["#ffecc7", "#ff9933"];
+  }
+}
