50f73c5e9b86000c3c7bc0773bfa8fc7095f00ee
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-05 18:06:34 +0300

    Adding the tab icons and deleting unnecessary screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,50 @@
 // app/_layout.tsx
 import { LightingProvider } from "@/context/LightingContext";
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
     <LightingProvider>
-      <Tabs>
-        <Tabs.Screen name="index" options={{ title: "Light" }} />
-        <Tabs.Screen name="settings" options={{ title: "Settings" }} />
-        <Tabs.Screen name="timer" options={{ title: "Timer" }} />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#ff9933",
+          tabBarInactiveTintColor: "gray",
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Light",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="sunny-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="mood"
+          options={{
+            title: "Mood",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="color-palette-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="settings-outline" size={size} color={color} />
+            ),
+          }}
+        />
       </Tabs>
     </LightingProvider>
   );
