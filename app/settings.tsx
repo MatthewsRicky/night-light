38f35dc6af2cfcc533a960191f93b34e34c75e5b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-05 18:15:13 +0300

    Bilding out the settings screen

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,11 +1,35 @@
-// rnf
-import React from "react";
+import FadeOutTimer from "@/components/FadeoutTimer";
+import ModeToggle from "@/components/ModeToggle";
+import WarmthSlider from "@/components/WarmthSlider";
 import { Text, View } from "react-native";
 
-export default function settings() {
+export default function SettingsScreen() {
   return (
-    <View>
-      <Text>settings</Text>
+    <View className="flex-1 bg-white dark:bg-black p-4">
+      <Text className="text-xl font-semibold mb-4 text-black dark:text-white">
+        Settings
+      </Text>
+
+      <View className="mb-8">
+        <Text className="text-lg font-semibold mb-2 text-black dark:text-white">
+          Warmth
+        </Text>
+        <WarmthSlider />
+      </View>
+
+      <View className="mb-8">
+        <Text className="text-lg font-semibold mb-2 text-black dark:text-white">
+          Auto-Off Timer
+        </Text>
+        <FadeOutTimer />
+      </View>
+
+      <View className="mb-8">
+        <Text className="text-lg font-semibold mb-2 text-black dark:text-white">
+          Mode
+        </Text>
+        <ModeToggle />
+      </View>
     </View>
   );
 }
