b13eece057ec15b940966dbc3b8ce57b15678117
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-05 18:16:06 +0300

    Boiler plate ambient light component

---
+++ components/AmbientLight.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AmbientLight() {
+  return (
+    <View>
+      <Text>AmbientLight</Text>
+    </View>
+  )
+}
