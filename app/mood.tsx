0b22a0c4809c62db6b5199a8403b38bdd140c111
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-08 23:01:35 +0300

    Adding a safe area view

--- app/mood.tsx
+++ app/mood.tsx
@@ -1,3 +1,12 @@
 // app/mood.tsx
+import React from "react";
+import { SafeAreaView, View } from "react-native";
 import MoodSelector from "../components/MoodSelector";
-export default MoodSelector;
+
+export default function mood() {
+  return (
+    <SafeAreaView className="flex-1 bg-blue-300/40">
+      <MoodSelector />
+    </SafeAreaView>
+  );
+}
