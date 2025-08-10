76b3de5aa3965741b05a8f914bedbadd63e3f2cd
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-10 15:56:26 +0300

    Globals css import, function TabBarIcon, refactoring the tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,57 @@
-// app/_layout.tsx
 import { LightingProvider } from "@/context/LightingContext";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import "./globals.css";
+
+function TabBarIcon({
+  name,
+  color,
+  size,
+}: {
+  name: any;
+  color: string;
+  size: number;
+}) {
+  return <Ionicons name={name} size={size} color={color} />;
+}
 
 export default function Layout() {
   return (
     <LightingProvider>
       <Tabs
         screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "#f59e0b", // amber-400
+          tabBarActiveTintColor: "#f59e0b",
           tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: { fontSize: 12, fontWeight: "500" },
         }}
       >
         <Tabs.Screen
           name="index"
           options={{
             title: "Light",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="sunny-outline" size={size} color={color} />
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <TabBarIcon name="sunny-outline" {...props} />
             ),
           }}
-          
         />
         <Tabs.Screen
           name="mood"
           options={{
             title: "Mood",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons
-                name="color-palette-outline"
-                size={size}
-                color={color}
-              />
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <TabBarIcon name="color-palette-outline" {...props} />
             ),
           }}
         />
-
         <Tabs.Screen
           name="settings"
           options={{
             title: "Settings",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="settings-outline" size={size} color={color} />
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <TabBarIcon name="settings-outline" {...props} />
             ),
           }}
         />
