53dcb92d26f479adbc64e93369ddd0aa4993c09c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-21 01:17:57 +0300

    UIContext for showing and hiding the tabBar

--- context/UIContext.tsx
+++ context/UIContext.tsx
@@ -1,24 +1,32 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+// context/UIContext.tsx
+import React, { createContext, useContext, useState } from "react";
 
 type UIContextType = {
   tabBarVisible: boolean;
-  setTabBarVisible: (visible: boolean) => void;
+  hideTabBar: () => void;
+  showTabBar: () => void;
 };
 
 const UIContext = createContext<UIContextType | undefined>(undefined);
-export const UIProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+
+export const UIProvider = ({ children }: { children: React.ReactNode }) => {
   const [tabBarVisible, setTabBarVisible] = useState(true);
 
   return (
-    <UIContext.Provider value={{ tabBarVisible, setTabBarVisible }}>
+    <UIContext.Provider
+      value={{
+        tabBarVisible,
+        hideTabBar: () => setTabBarVisible(false),
+        showTabBar: () => setTabBarVisible(true),
+      }}
+    >
       {children}
     </UIContext.Provider>
   );
 };
-export const useUI = (): UIContextType => {
+
+export const useUI = () => {
   const context = useContext(UIContext);
-  if (!context) {
-    throw new Error("useUI must be used within a UIProvider");
-  }
+  if (!context) throw new Error("useUI must be used within UIProvider");
   return context;
 };
