0ec64d3bacb997e739c076c6ba25951547ac5c45
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-21 01:14:13 +0300

    clean up

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,11 @@
 // app/index.tsx
 import StartScreen from "@/components/StartScreen";
 import React from "react";
-import { View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1">
+    <>
       <StartScreen />
-    </View>
+    </>
   );
 }
