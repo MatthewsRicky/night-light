9a1d03c832c6870b0d35afd67139c0a79c2ed613
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-08-25 22:12:33 +0300

    Simple edit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
-// app/index.tsx
-import StartScreen from "@/components/StartScreen";
 import React from "react";
+import { SafeAreaView, View } from "react-native";
+import StartScreen from "@/components/StartScreen";
 
 export default function Home() {
   return (
-    <>
-      <StartScreen />
-    </>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
+      <View style={{ flex: 1 }}>
+        <StartScreen />
+      </View>
+    </SafeAreaView>
   );
 }
