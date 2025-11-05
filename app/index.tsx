73d4f27a7eab609a260908051f7182e1f1c52a95
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-11-05 22:05:54 +0300

    Fixing the splash

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,20 @@
 import StartScreen from "@/components/StartScreen";
-import React from "react";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
 import { SafeAreaView, View } from "react-native";
 
 export default function Home() {
+  // Prevent splash from auto-hiding
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    async function prepare() {
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // simulate load
+      await SplashScreen.hideAsync(); // hide when ready
+    }
+
+    prepare();
+  }, []);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
       <View style={{ flex: 1 }}>
